c783d152f9e635a235c62668da42af6afb318a7e
Brunovsky <elbrunovsky@gmail.com>
2018-09-28 17:27:40 +0100

    Cleared MySceneGraph.js, specified TODO.md

--- MySceneGraph.js
+++ MySceneGraph.js
@@ -2,24 +2,12 @@
  * MySceneGraph class, representing the scene graph.
  */
 class MySceneGraph {
-    /**
-     * @constructor
-     */
     constructor(filename, scene) {
         this.loadedOk = null;
 
         // Establish bidirectional references between scene and graph.
         this.scene = scene;
         scene.graph = this;
-
-        this.nodes = [];
-
-        this.idRoot = null;                    // The id of the root element.
-
-        this.axisCoords = [];
-        this.axisCoords['x'] = [1, 0, 0];
-        this.axisCoords['y'] = [0, 1, 0];
-        this.axisCoords['z'] = [0, 0, 1];
 
         // File reading 
         this.reader = new CGFXMLreader();
@@ -29,18 +17,14 @@ class MySceneGraph {
          * After the file is read, the reader calls onXMLReady on this object.
          * If any error occurs, the reader calls onXMLError on this object, with an error message
          */
-
         this.reader.open('scenes/' + filename, this);
     }
 
-    /*
-     * Callback to be executed after successful reading
-     */
     onXMLReady() {
-        this.log("XML Loading finished.");
+        console.log("XML Loading finished.");
         let rootElement = this.reader.xmlDoc.documentElement;
 
-        // Here should go the calls for different functions to parse the various blocks
+        console.log()
         let error = this.parseXMLFile(rootElement);
 
         if (error) return;
@@ -59,7 +43,7 @@ class MySceneGraph {
         console.log(rootElement);
 
         try {
-            var yas = new XMLYas(rootElement);
+            this.yas = new XMLYas(rootElement);
         } catch (e) {
             if (e instanceof XMLException) {
                 console.log(e);
@@ -69,33 +53,8 @@ class MySceneGraph {
             }
         }
 
-        console.log(yas);
+        console.log(this.yas);
 
         return false;
-    }
-
-    /*
-     * Callback to be executed on any read error, showing an error on the console.
-     * @param {string} message
-     */
-    onXMLError(message) {
-        console.error("XML Loading Error: " + message);
-        this.loadedOk = false;
-    }
-
-    /**
-     * Callback to be executed on any message.
-     * @param {string} message
-     */
-    log(message) {
-        console.log("   " + message);
-    }
-
-    /**
-     * Displays the scene, processing each node, starting in the root node.
-     */
-    displayScene() {
-        // entry point for graph rendering
-        //TODO: Render loop starting at root of graph
     }
 }
--- TODO.md
+++ TODO.md
@@ -6,6 +6,50 @@
 > Emitir warnings para valores imprevistos, resolver referências, construir grafo
 > Detetar ciclos e components não alcançáveis
 
+; scene:
+	root component must exist
+	axis_length > 0.
+; views:
+	default must exist
+	must have at least one view
+	near < far
+	from != to
+; ambient:
+	r, g, b, a in the range [0, 1].
+; lights:
+	r, g, b, a in the range [0, 1].
+	location != target
+; texture:
+	open associated file, give a warning if it
+	does not exist
+; material:
+	shininess > 0
+	r, g, b, a in the range [0, 1].
+; transformation:
+	warning if angle is not in the range [-360, 360].
+	scale's x, y, z should not be 0.
+; primitive
+	radius, height, slices, stacks, loops, inner, outer
+	should all be positive
+	...
+; component
+	; transformation
+		transformationref must exist
+		same as transformation above
+	; material
+		distinguish id="inherit"
+		otherwise verify material with the id exists
+	; texture
+		distinguish id="inherit" and id="none"
+		otherwise verify texture with the id exists
+		length_s and length_t should be positive
+	; children
+		componentref/primitiveref with given ids must exist
+		verify all children are reachable (hard) (emit warning)
+		verify there are no cycles (hard) (throw exception)
+		in my opinion, do not emit a warning if a primitive
+		is not used
+
 3ª TRADUÇÃO (SceneGraph --> WebGL)
 > Baseado em CGRA
 > Materials stack
