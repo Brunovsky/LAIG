4e3dfda58873c6f821513ba0bcadfcbb05e6b427
Brunovsky <elbrunovsky@gmail.com>
2018-10-08 18:01:09 +0100

    Fixed staircase.xml

--- core/globals.js
+++ core/globals.js
@@ -8,7 +8,7 @@ const DISABLED_LIGHTS_VISIBLE = false;
 
 const DEBUG_DISPLAY = true;
 
-const DEFAULT_YAS_FILE = "scenes/rubiks.xml";
+const DEFAULT_YAS_FILE = "scenes/staircase.xml";
 
 const LIGHT_CONSTANT_ATTENUATION = 0.5;
 
