f8bfd527f7abcebe7cce8a2770ee5d2884b9f410
DanielGanso <DanielGanso@github.com>
2018-11-08 15:40:36 +0000

    linear animation preparation

--- Animation/LinearAnimation.js
+++ Animation/LinearAnimation.js
@@ -1,33 +1,45 @@
-class LinearAnimation extends Animation{
-    constructor(scene, time, cp = {}, span){
+class LinearAnimation extends Animation {
+    constructor(scene, cp = {}, span) {
         super(scene);
         this.cp = cp;
+        this.elapsed_time = 0;
         this.span = span;
-        this.dist = 0;
-        this.transx = 0;
-        this.transy = 0;
-        this.transz = 0;
-        
+        vectors();
+    }
 
-        this.inc = this.dist/time;
-        
-    }    
-
-    rotateAngle(point1, point2, vector = [0,0,1]){
-        x = point2[0] - point1[0];
-        z = point2[2] - point1[2];
-        vecnorm = Math.sqrt(x*x + z*z);
-        norm = Math.sqrt(vector[0]*vector[0] + vector[1]*vector[1] + vector[2]*vector[2]);
+    /** rotateAngle(vector1, vecto2r = [0,0,1]){
+        let vecnorm = Math.sqrt(x*x + z*z);
+        let norm = Math.sqrt(vector[0]*vector[0] + vector[1]*vector[1] + vector[2]*vector[2]);
         return Math.acos((x*vector[0] + z*vector[2])/(vecnorm*norm));
+    }*/
+
+    vectors() {
+        this.vec = [];
+        for (let i = 0; cp.length > i; i++) {
+            this.vec = new Array(3);
+            this.vec[i][0] = cp[i + 1][0] - cp[i][0];
+            this.vec[i][1] = cp[i + 1][1] - cp[i][1];
+            this.vec[i][2] = cp[i + 1][2] - cp[i][2];
+        }
     }
 
+    update(currTime) {
 
-    update(currTime){
-
+        this.elapsed_time += currTime - this.previousTime;
+        let percentage = (this.elapsed_time / 1000) / this.span;
+        let mid_point = cp.length * percentage;
+        let point = Math.floor(mid_point);
+        let k = mid_point - point;
+        let translate = {x: cp[point][0]+ k*this.vec[point][0], 
+            y: cp[point][1]+ k*this.vec[point][1],
+            z: cp[point][2]+ k*this.vec[point][2]
+        }
     }
 
-    apply(){
-        
+    apply() {
+
+        this.scene.translate(this.translate.x,this.translate.y, this.translate.z);
+
     }
 
 
